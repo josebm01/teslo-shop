6de8dc6fbbfcf806e6f199ccd9d50edf9657f9c9
Jose BM <jbaxin@manuelsolis.com>
2024-05-10 10:48:59 -0600

    Se inicia menu lateral y se agregan las imágenes de los productos, así como la segunda imagen se muestra cuando se posiciona sobre la imagen del producto

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,8 @@
+export { Sidebar } from "./sidebar/Sidebar";
+
+export { ProductGridItem } from "./products/product-grid/ProductGridItem";
+export { ProductGrid } from "./products/product-grid/ProductGrid";
+
 export { Title } from "./ui/title/Title";
 export { PageNotFound } from "./ui/not-found/PageNotFound";
 export { TopMenu } from "./ui/top-menu/TopMenu";
--- src/seed/seed.ts
+++ src/seed/seed.ts
@@ -1,21 +1,7 @@
-interface SeedProduct {
-    description: string;
-    images: string[];
-    inStock: number;
-    price: number;
-    sizes: ValidSizes[];
-    slug: string;
-    tags: string[];
-    title: string;
-    type: ValidTypes;
-    gender: 'men'|'women'|'kid'|'unisex'
-}
-
-type ValidSizes = 'XS'|'S'|'M'|'L'|'XL'|'XXL'|'XXXL';
-type ValidTypes = 'shirts'|'pants'|'hoodies'|'hats';
+import { Product } from "@/interfaces";
 
 interface SeedData {
-    products: SeedProduct[],
+    products: Product[],
 }
 
 
