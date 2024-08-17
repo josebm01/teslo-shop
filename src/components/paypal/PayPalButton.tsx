5da785d6fd625f28fda19074e396b19a9e0fc101
Jose BM <jbaxin@manuelsolis.com>
2024-08-17 11:12:29 -0600

    Se agrega hook de paypal para mostrar un skeleton mientras carga el botón de pago

--- src/components/paypal/PayPalButton.tsx
+++ src/components/paypal/PayPalButton.tsx
@@ -1,10 +1,21 @@
 'use client'
 
-import { PayPalButtons } from '@paypal/react-paypal-js'
+import { PayPalButtons, usePayPalScriptReducer } from '@paypal/react-paypal-js'
 import React from 'react'
 
 export const PayPalButton = () => {
+  
+  const [{ isPending }] = usePayPalScriptReducer()
+
+  if ( isPending ){
+    return(
+      <div className='animate-pulse'>
+        <div className='h-8 bg-gray-300 rounded' />
+      </div>
+    )
+  }
+
   return (
     <PayPalButtons style={{ layout: "horizontal" }} />
-    )
+  )
 }
