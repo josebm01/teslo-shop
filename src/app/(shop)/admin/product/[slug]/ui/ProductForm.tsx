9ba89ef012cf75afc17822dc98e9fdfcb8c3e80b
Jose BM <jbaxin@manuelsolis.com>
2024-08-29 15:30:13 -0600

    Se carga formulario del producto

--- src/app/(shop)/admin/product/[slug]/ui/ProductForm.tsx
+++ src/app/(shop)/admin/product/[slug]/ui/ProductForm.tsx
@@ -1,27 +1,62 @@
 "use client";
 
 import { CategoryProduct, SeedProduct } from "@/interfaces";
+import { useForm } from "react-hook-form";
 
 interface Props {
   product: SeedProduct;
   categories: CategoryProduct[];
 }
 
+interface FormInputs {
+  title: string;
+  slug: string;
+  description: string;
+  price: number;
+  inStock: number;
+  sizes: string[];
+  tags: string;
+  gender: 'men'|'women'|'kid'|'unisex';
+  categoryId: string;
+}
+
+
 const sizes = ["XS", "S", "M", "L", "XL", "XXL"];
 
 export const ProductForm = ({ product, categories }: Props) => {
+
+  const { 
+    handleSubmit, 
+    register,
+    formState: { isValid }
+  } = useForm<FormInputs>({
+    defaultValues: {
+      ...product,
+      tags: product.tags.join(', '),
+      sizes: product.sizes ?? []
+    }
+  })
+
+
+  const onSubmit = async ( data: FormInputs) => {
+
+  }
+
   return (
-    <form className="grid px-5 mb-16 grid-cols-1 sm:px-0 sm:grid-cols-2 gap-3">
+    <form 
+      onSubmit={ handleSubmit(onSubmit) } 
+      className="grid px-5 mb-16 grid-cols-1 sm:px-0 sm:grid-cols-2 gap-3"
+    >
       {/* Textos */}
       <div className="w-full">
         <div className="flex flex-col mb-2">
           <span>Título</span>
-          <input type="text" className="p-2 border rounded-md bg-gray-200" />
+          <input type="text" className="p-2 border rounded-md bg-gray-200" { ...register('title', { required: true }) } />
         </div>
 
         <div className="flex flex-col mb-2">
           <span>Slug</span>
-          <input type="text" className="p-2 border rounded-md bg-gray-200" />
+          <input type="text" className="p-2 border rounded-md bg-gray-200" { ...register('slug', { required: true }) } />
         </div>
 
         <div className="flex flex-col mb-2">
@@ -29,22 +64,23 @@ export const ProductForm = ({ product, categories }: Props) => {
           <textarea
             rows={5}
             className="p-2 border rounded-md bg-gray-200"
+            { ...register('description', { required: true }) }
           ></textarea>
         </div>
 
         <div className="flex flex-col mb-2">
           <span>Price</span>
-          <input type="number" className="p-2 border rounded-md bg-gray-200" />
+          <input type="number" className="p-2 border rounded-md bg-gray-200" { ...register('price', { required: true, min: 0 }) } />
         </div>
 
         <div className="flex flex-col mb-2">
           <span>Tags</span>
-          <input type="text" className="p-2 border rounded-md bg-gray-200" />
+          <input type="text" className="p-2 border rounded-md bg-gray-200" { ...register('tags', { required: true }) } />
         </div>
 
         <div className="flex flex-col mb-2">
           <span>Gender</span>
-          <select className="p-2 border rounded-md bg-gray-200">
+          <select className="p-2 border rounded-md bg-gray-200" { ...register('gender', { required: true }) }>
             <option value="">[Seleccione]</option>
             <option value="men">Men</option>
             <option value="women">Women</option>
@@ -55,7 +91,7 @@ export const ProductForm = ({ product, categories }: Props) => {
 
         <div className="flex flex-col mb-2">
           <span>Categoria</span>
-          <select className="p-2 border rounded-md bg-gray-200">
+          <select className="p-2 border rounded-md bg-gray-200" { ...register('categoryId', { required: true }) }>
             <option value="">[Seleccione]</option>
             {
               categories.map( category => (
